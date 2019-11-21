209a7e6260480ac1a4b44e9aaad5beb9b50b1d1c
Chegrani otmane <chegraniotmane@gmail.com>
2019-11-21 09:32:12 +0100

    Fix ProtegeDto

--- src/app/models/ProtegeDto.ts
+++ src/app/models/ProtegeDto.ts
@@ -1,6 +1,8 @@
 export class ProtegeDto {
     // tslint:disable: variable-name
     private _tel: string;
+    private _groupeId: number;
+
 
     constructor() { }
 
@@ -9,5 +11,11 @@ export class ProtegeDto {
     }
     public set tel(value: string) {
         this._tel = value;
+    }
+    public get groupeId(): number {
+        return this._groupeId;
+    }
+    public set groupeId(value: number) {
+        this._groupeId = value;
     }
 }
